585e9a566220048f01388f8f215ce3e3b7e85d9e
Katherine <freepthanq1@gmail.com>
2025-02-03 17:04:40 +0200

    appearance of the materials block

--- js/script.js
+++ js/script.js
@@ -25,3 +25,34 @@ window.addEventListener('scroll', function () {
     // Додаємо або видаляємо клас 'scroll', залежно від того, чи є скрол
     document.querySelector('.header').classList.toggle('scroll', scrollTop > 0);
 });
+
+
+window.addEventListener('scroll', function () {    
+    let windowHeight = window.innerHeight; // Висота блоку header
+    let maxScroll = windowHeight * 0.01; // Різниця між висотою вікна і висотою header
+
+    // Отримуємо поточний скрол
+    let scrollTop = window.scrollY;
+
+    // Обчислюємо відсоток прокрутки (від 0 до 1)
+    let percentage = Math.min(scrollTop / maxScroll, 1); 
+
+    // Задаємо CSS змінну для непрозорості фону
+    document.documentElement.style.setProperty('--scroll-opacity-before', percentage);
+});
+
+
+window.addEventListener('scroll', function () {    
+    let windowHeight = window.innerHeight; // Висота блоку header
+    let maxScroll = windowHeight * 0.008; // Різниця між висотою вікна і висотою header
+
+    // Отримуємо поточний скрол
+    let scrollTop = window.scrollY;
+
+    // Обчислюємо відсоток прокрутки (від 0 до 1)
+    let percentage = Math.min(scrollTop / maxScroll, 1); 
+
+    // Задаємо CSS змінну для непрозорості фону
+    document.documentElement.style.setProperty('--scroll-opacity-swiper', percentage);
+});
+
