7a4ceb7891da1087ad648843f56f1d715794666a
Katherine <freepthanq1@gmail.com>
2025-02-03 13:29:19 +0200

    shadowed header

--- js/script.js
+++ js/script.js
@@ -9,6 +9,19 @@ const swiper = new Swiper('.materials__slider', {
 
 });
 
-window.addEventListener('scroll', function () {
-    document.querySelector('.header').classList.toggle('scroll', scrollY > 0);
+window.addEventListener('scroll', function () {    
+    let windowHeight = window.innerHeight; // Висота блоку header
+    let maxScroll = windowHeight * 0.2; // Різниця між висотою вікна і висотою header
+
+    // Отримуємо поточний скрол
+    let scrollTop = window.scrollY;
+
+    // Обчислюємо відсоток прокрутки (від 0 до 1)
+    let percentage = Math.min(scrollTop / maxScroll, 1); 
+
+    // Задаємо CSS змінну для непрозорості фону
+    document.documentElement.style.setProperty('--scroll-opacity', percentage);
+
+    // Додаємо або видаляємо клас 'scroll', залежно від того, чи є скрол
+    document.querySelector('.header').classList.toggle('scroll', scrollTop > 0);
 });
