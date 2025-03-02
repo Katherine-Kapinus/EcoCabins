df7acae351fad27a34ba0655f6a5317aeba9256b
Katherine <freepthanq1@gmail.com>
2025-03-02 23:07:47 +0200

    fix opacity of swiper for main screen

--- js/script.js
+++ js/script.js
@@ -45,19 +45,22 @@ window.addEventListener('scroll', function () {
 });
 
 
-window.addEventListener('scroll', function () {    
-    let windowHeight = window.innerHeight; // Висота блоку header
-    let maxScroll = windowHeight * 0.008; // Різниця між висотою вікна і висотою header
+function updateOpacity() {    
+    let windowHeight = window.innerHeight; 
+    let maxScroll = windowHeight * 0.008; 
 
-    // Отримуємо поточний скрол
-    let scrollTop = window.scrollY;
-
-    // Обчислюємо відсоток прокрутки (від 0 до 1)
+    let scrollTop = window.scrollY; 
     let percentage = Math.min(scrollTop / maxScroll, 1); 
 
-    // Задаємо CSS змінну для непрозорості фону
     document.documentElement.style.setProperty('--scroll-opacity-swiper', percentage);
-});
+}
+
+// Викликаємо функцію одразу при завантаженні сторінки
+updateOpacity(); 
+
+// Додаємо слухач подій для скролу
+window.addEventListener('scroll', updateOpacity);
+
 
 document.querySelector('.header__burger').addEventListener('click', function() {
     this.classList.toggle('active'); // Додаємо/знімаємо клас .active у бургер-меню
